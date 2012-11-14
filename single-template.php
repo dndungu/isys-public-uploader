41c796801286069e92ac7d42dad11c27c17d47bd
David Njuguna <dnjuguna@gmail.com>
2012-11-15 00:28:40 +0300

    integrate

--- single-template.php
+++ single-template.php
@@ -119,6 +119,13 @@ $categories = wp_get_post_terms($post->ID, 'blog-indlaeg-kategori');
 								Recaptcha.create("6Lfgi9gSAAAAAOBUxMtjJlSd8PNn1sxQbgH1OP6e", document.getElementById('recaptcha_widget'), {theme: "custom"});
 							</script>					
 						</div>
+						<div class="row" style="font-family: Gill Sans W02,Arial,sans-serif;color:#9a9a9a;margin:150px 0 0 0;">
+							<strong>ANSVAR</strong>
+							<br/>
+							Det er tanken, at VGM værktøjskassen skal være dynamisk, og indholdet skal komme fra brugerne på sitet. Københavns Kommune tager således ikke ansvar for indlæggene på sitet.
+							Københavns Kommune forbeholder sig dog ret til at redigere og slette i indlæggene, hvis de vurderes at have stødende, fejlagtig eller injurierende karakter. Det håber vi dog ikke bliver tilfældet.						
+						</div>
+						
 					<?php }?>
 				<?php }?>
 			</div>
