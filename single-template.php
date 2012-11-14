2bd4b6a98e59625cc656b99f798bb2d8104a75cb
David Njuguna <dnjuguna@gmail.com>
2012-11-14 23:29:50 +0300

    integrate

--- single-template.php
+++ single-template.php
@@ -92,7 +92,7 @@ $categories = wp_get_post_terms($post->ID, 'blog-indlaeg-kategori');
 								<?php }?>
 							<?php }?>
 							<form method="POST" id="isys_visitor_comment_form">
-								<h2>TILFØJ KOMMENTAR</h2>
+								<h2 style="font-family:Gill Sans W02,Arial,sans-serif;">TILFØJ KOMMENTAR</h2>
 								<input type="hidden" name="action" value="isys_visitor_plugin"/>
 								<input type="hidden" name="do" value="create-comment"/>
 								<input type="hidden" name="post_id" value="<?php the_ID()?>"/>
