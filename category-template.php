8e44ca7c934611a59579ce6948fbe1ae383b3a71
David Njuguna <dnjuguna@gmail.com>
2012-12-05 00:06:19 +0300

    integrate

--- category-template.php
+++ category-template.php
@@ -74,7 +74,7 @@ get_header();
 									<?php }?>
 								</span>
 								<br/>
-								<?php setlocale(LC_ALL, 'it_IT');?>
+								<?php setlocale(LC_ALL, 'da_DK');?>
 								<span style="text-transform:lowercase;font-size:12px;"><?php print strftime('%A %e. %B %Y', get_the_time('U'))?></span>
 								<br/>
 								<span style="font-size:12px;">
